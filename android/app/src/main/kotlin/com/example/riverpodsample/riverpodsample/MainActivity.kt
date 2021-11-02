a067a3b4099e34a6a25181100cd2ae9c14c24cf1
sakushin <sakushin.udemy@gmail.com>
2021-11-02 20:13:48 +0900

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/riverpodsample/riverpodsample/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.riverpodsample.riverpodsample
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+void main() {
+  runApp(ProviderScope(child: const MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  Provider<String> _provider =
+      Provider((ref) => 'You have pushed the button this many times:');
+
+  StateProvider<int> _stateProvider = StateProvider((ref) => 0);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Consumer(
+              builder: (context, ref, child) => Text(
+                ref.watch(_provider),
+              ),
+            ),
+            Consumer(
+              builder: (context, ref, child) => Text(
+                '${ref.watch(_stateProvider).state}',
+                style: Theme.of(context).textTheme.headline4,
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: Consumer(
+        builder: (context, ref, child) => FloatingActionButton(
+          onPressed: () => ref.read(_stateProvider).state++,
+          tooltip: 'Increment',
+          child: const Icon(Icons.add),
+        ),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
